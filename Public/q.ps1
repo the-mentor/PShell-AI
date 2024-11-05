02619c6f80b20800651329d8f9e9013664c84867
Avri Chen-Roth <the-mentor@users.noreply.github.com>
2024-11-05 14:42:22 +0200

    Add clipboard support for macOS and Linux in q function

--- Public/q.ps1
+++ Public/q.ps1
@@ -40,7 +40,17 @@ However, if the user is clearly asking a question then answer it very briefly an
             else {
                 Write-Host -ForegroundColor Green "Copied to clipboard."
             }
-            $agentResponse | clip
+            
+            if($IsMacOS) {
+                $agentResponse | pbcopy
+            }
+            if($IsLinux) {
+                if(Get-Command xclip -ErrorAction SilentlyContinue){
+                    $agentResponse | xclip -selection clipboard
+                }
+            }
+            else {
+                $agentResponse | clip
             break            
         }
     }
